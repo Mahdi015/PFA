1a34e748145896a9dd0d450146bb2b726f086b6e
mahdi <ferianimahdi93@gmail.com>
2024-06-06 15:38:33 +0100

    fix cors

--- back/src/index.ts
+++ back/src/index.ts
@@ -5,7 +5,8 @@ import userRoutes from './routes/userRoutes';
 const PORT = 3001;
 
 const app = express();
-
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 app.use('/users', userRoutes);
 
